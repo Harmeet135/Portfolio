662281f909cadcc47ec8707cee754031780ca9c4
Harmeet <harmeetchahal9891@gmail.com>
2023-04-02 21:45:05 +0530

    2nd commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,17 +36,14 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+     
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Harmeet",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "harmeetchahal9891@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
